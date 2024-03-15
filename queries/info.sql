ed27ec3e8e06baab54439398bfb0eea73a025dc0
esmaeelE <esmaeelEE@protonmail.com>
2024-03-15 15:11:07 +0330

    sql qurey to get info
    Information about db

---
+++ queries/info.sql
@@ -0,0 +1,17 @@
+-- Some query to get overal information from Database
+
+-- Database sizes
+SELECT 
+    table_schema 'Database Name', 
+    SUM(data_length + index_length) 'Size (Bytes)', 
+    ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) 'Size (MiB)', 
+    ROUND(SUM(data_length + index_length) / 1024 / 1024 / 1024, 2) 'Size (GiB)' 
+FROM information_schema.tables 
+-- WHERE table_schema = 'mysql'
+GROUP BY table_schema;
+
+-- Table size, for specific database_name: mysql
+SELECT table_name AS "Table", ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)"
+FROM information_schema.TABLES
+WHERE table_schema = "mysql";
+
